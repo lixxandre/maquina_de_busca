fa8bb34a3208a9ab6ed230ec6b7910d5989fabd0
Alexandre Nascimento Junior <57258128+lixxandre@users.noreply.github.com>
2019-11-23 21:11:42 -0300

    Update teste.cpp

--- teste.cpp
+++ teste.cpp
@@ -9,48 +9,84 @@
 
 using std::string;
 
-TEST_SUITE("MaquinaDeBusca"){
-    TEST_CASE("Tratamento(string)"){
-        string x = "B1";
-        string saida = MaquinaDeBusca::Tratamento(x);
-        CHECK(saida == "b");
-        x = "33";
-        saida = MaquinaDeBusca::Tratamento(x);
-        CHECK(saida == "3"); 
+TEST_SUITE("MaquinaDeBusca") {
+    TEST_CASE("Tratamento"){
+        string a = "OI";
+        string b = "VOCE";
+        string c = "DIA.?";
+
+        Tratamento(a);
+        Tratamento(b);
+        Tratamento(c);
+
+        CHECK(a == "oi");
+        CHECK(b == "voce");
+        CHECK(c == "dia");
+    }
+    TEST_CASE("split"){
+        string a = "guarda-chuvas";
+        string b = "oi.oi";
+        string c = "dia-a-dia?";
+
+        split(a);
+        split(b);
+        split(c);
+
+        CHECK(a == "guardachuvas");
+        CHECK(b == "oioi");
+        CHECK(c == "diaadia");
     }
 
-    TEST_CASE("ler_arquivo(string)"){
+    TEST_CASE("frequenciaCount"){
+        string a[5], b[5];
+        int c = 0;
+        a[0] = "oi";
+        a[1] = "oi";
+        a[2] = "tudo";
+        a[3] = "bem";
+        a[4] = "bem";
+
+        for(int i = 0; i < 5; i++){
+            b[i] = a[i];
+        }
+
+        frequenciaCount(5, a, b, c);
+
+        CHECK(c == 2);
     }
 
-    TEST_CASE("calc_importancia()"){
+    TEST_CASE("calc_importancia"){
+        float N = 5;
+        float nx = 2;
+        float x;
+        MaquinaDeBusca::calc_importancia(N,nx);
+        CHECK(x == (0,39794));
 
-        int x= MaquinaDeBusca::calc_importancia(2,1);
-        CHECK(x == 3);
-        
-        x = MaquinaDeBusca::calc_importancia(4,2);
-        CHECK(x == 3);
+        N = 4;
+        nx = 2;
+        CHECK(MaquinaDeBusca::calc_importancia(N,nx) == (0.30103));
     }
 
-    TEST_CASE("produto_interno(vector)"){
-
+    TEST_CASE("produto_interno"){
+        vector<float> q;
+        vector<float> d;
+        d = {1, 2, 3};
+        q = {1, 5, 7};
+        CHECK(MaquinaDeBusca::produto_interno(q, d); == (32));  
     }
-/*
-    TEST_CASE("norma(vector)"){
-        vector<float> v { 1, 2, 3};
-        CHECK(MaquinaDeBusca::norma(v) == 14);
+
+    TEST_CASE("norma"){
+        vector<float> v;
+        v = {4, 4, 2};
+        MaquinaDeBusca::norma(v);
+        CHECK(norma(v) == (6));
     }
 
-    TEST_CASE("similaridade()"){
-        float normaQ, normaD, QdotD;
-        
-        QdotD = 9;
-        normaQ = (3*(sqrt(2)));
-        normaD = 3;
-        float x = MaquinaDeBusca::similaridade(normaQ, normaD, QdotD);
-        CHECK(x == 2/sqrt(2));
-
+    TEST_CASE("similaridade"){
+        float Q,D,QD;
+        Q=6;
+        D=9;
+        QD=10
+        CHECK(MaquinaDeBusca::similaridade(Q,D,QD) == (0.18518))
     }
-*/ 
 }
-
-
